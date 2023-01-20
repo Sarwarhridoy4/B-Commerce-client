41e8f94783f5b70e22214f15248fcf4d725a4d4c
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2023-01-20 17:31:56 +0600

    Basic routing completed

--- src/Layouts/Main/Main.jsx
+++ src/Layouts/Main/Main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from "../../Components/Shared/Navbar/Navbar" 
+import Footer from "../../Components/Shared/Footer/Footer" 
 
 const Main = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
