ae1bae86cfe39d08b063ce9a9ebe0aa12941f54f
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2023-01-21 13:18:42 +0600

    Homepage added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import router from './Routes/Routes/Routes';
 function App() {
   return (
     <div className="App">
-      <div className='max-w-[1440px] mx-auto'>
+      <div className='max-w-[1440px] mx-auto bg-gradient-to-r from-green-500 via-green-300'>
       <RouterProvider router={router}></RouterProvider>
     </div>
     </div>
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,10 +1,70 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            <p>This is Home Page</p>
+        <div class="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center ">
+        <div class="w-full lg:w-1/2">
+            <div class="lg:max-w-lg">
+                <h1 class="text-3xl font-semibold tracking-wide text-gray-800 lg:text-4xl">Find garden fresh Vegetable Straight to your hand</h1>
+                <p class="mt-4 text-gray-600 ">We work with the best fermers to ensure best can be deliverd to your kitchen</p>
+                <div class="grid gap-6 mt-8 sm:grid-cols-2">
+                    <div class="flex items-center text-gray-800 -px-3">
+                        <svg class="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+
+                        <span class="mx-3">Garden Fresh</span>
+                    </div>
+
+                    <div class="flex items-center text-gray-800 -px-3">
+                        <svg class="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+
+                        <span class="mx-3">Organic Product</span>
+                    </div>
+
+                    <div class="flex items-center text-gray-800 -px-3">
+                        <svg class="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+
+                        <span class="mx-3">100% free of chemical</span>
+                    </div>
+
+                    <div class="flex items-center text-gray-800 -px-3">
+                        <svg class="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+
+                        <span class="mx-3">DoorStep Delivery</span>
+                    </div>
+
+                    <div class="flex items-center text-gray-800 -px-3">
+                        <svg class="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+
+                        <span class="mx-3">Payment Security</span>
+                    </div>
+
+                    <div class="flex items-center text-gray-800 -px-3">
+                        <svg class="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+
+                        <span class="mx-3">Fast shipping (+ Express)</span>
+                    </div>
+                    </div>
+                    <Link to='products'><button className="btn btn-outline bg-yellow-500 mt-5 border-none">Explore Now</button></Link>
+            </div>
         </div>
+
+        <div class="flex items-center justify-center w-full h-96 lg:w-1/2">
+            <img class="object-cover w-full h-full max-w-2xl rounded-md" src="https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467_960_720.jpg" alt="glasses"/>
+        </div>
+    </div>
     );
 };
 
